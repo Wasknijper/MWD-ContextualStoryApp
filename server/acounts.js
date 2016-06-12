b383c14d6218dff877c4c8a563f0493e000fae9e
Wasknijper <mudkippers@hotmail.com>
2016-06-12 23:34:39 +0200

    request permissions

--- server/acounts.js
+++ server/acounts.js
@@ -12,9 +12,3 @@ Meteor.users.deny({
     return true;
   }
 });
-
-Accounts.ui.config({
-  requestPermissions: {
-     facebook: ['public_profile', 'user_actions.books', 'user_actions.music', 'user_birthday', 'user_education_history', 'user_hometown', 'user_likes', 'user_location', 'user_work_history'],
-  }
-});
